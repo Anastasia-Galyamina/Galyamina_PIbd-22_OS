8d3f39c368af005b5533bab7425322d86f39e06f
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2020-03-20 13:04:32 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+	private static ArrayList<Process> Processes = new ArrayList<Process>();
+	private static Random rand = new Random();
+	private static int quant = 10;
+
+	public static void main(String[] args) {
+		createProcess();
+		getInfo();
+		Implement();
+	}
+
+	private static void Implement() {
+		if (quant < 1) {
+			System.out.println("Квант времени меньше 1");
+			return;
+		}
+		while (!Processes.isEmpty()) {
+			int sizeProcesses = Processes.size();
+			for (int i = 0; i < sizeProcesses; i++) {
+				if (Processes.get(i).HaveTime()) {
+					if (!Processes.get(i).isEmpty()) {
+						Processes.get(i).implementThreads();
+					} else {
+						System.out.println("Все потоки " + Processes.get(i).getDescription() + "  выполнены");
+						Processes.remove(i);
+						sizeProcesses = Processes.size();
+						i--;
+					}
+				} else {
+					Processes.remove(i);
+					sizeProcesses = Processes.size();
+					i--;
+				}
+			}
+		}
+		System.out.println("Все процессы выполнены");
+	}
+
+	private static void getInfo() {
+		for (int i = 0; i < Processes.size(); i++) {
+			System.out.println(Processes.get(i).getDescription() + " Потоков: " + Processes.get(i).getCount());
+		}
+		
+		System.out.println();
+	}
+
+	private static void createProcess() {		
+	
+		for (int i = 0; i < rand.nextInt(5) + 3; i++) {
+			Processes.add(new Process("" + i, quant, Priority.values()[rand.nextInt(3)]));
+		}
+	}
+}
+
