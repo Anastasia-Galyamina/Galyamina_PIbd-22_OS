19dd849d8a03cc421d860b2d242a04f4785e031d
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2020-04-24 14:14:04 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,86 +1,23 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main {
-	private static ArrayList<Process> Processes = new ArrayList<Process>();
-	private static Random rand = new Random();
-	private static int quant = 10;
 
 	public static void main(String[] args) {
-		createProcess();
-		getInfo();
-		Implement();
-	}
-
-	private static void Implement() {
-		if (quant < 1) {
-			System.out.println("Квант времени меньше 1");
-			return;
-		}
-		while (!Processes.isEmpty()) {
-			int sizeProcesses = Processes.size();
-			for (int i = 0; i < sizeProcesses; i++) {
-				if (Processes.get(i).HaveTime()) {
-					if (!Processes.get(i).isEmpty()) {
-						Processes.get(i).implementThreads();
-					} else {
-						System.out.println("Все потоки " + Processes.get(i).getDescription() + "  выполнены");
-						Processes.remove(i);
-						sizeProcesses = Processes.size();
-						i--;
-					}
-				} else {
-					Processes.remove(i);
-					sizeProcesses = Processes.size();
-					i--;
-				}
-			}
-		}
-		System.out.println("Все процессы выполнены");
-	}
-
-	private static void getInfo() {
-		for (int i = 0; i < Processes.size(); i++) {
-			System.out.println(Processes.get(i).getDescription() + " Потоков: " + Processes.get(i).getCount());
-		}
+		Random rnd = new Random();
+		int pages[] = new int[32];    // массив виртуальных страниц процесса, к которым он обращается 
+		ArrayList<Record> Table = new ArrayList<>(); //таблица соответствия виртуальных адресов процесса и физических
 		
-		System.out.println();
-	}
-
-	private static void createProcess() {		
-	
-		for (int i = 0; i < rand.nextInt(5) + 3; i++) {
-			Processes.add(new Process("" + i, quant, Priority.values()[rand.nextInt(3)]));
-		}
+		for(int i = 0; i < 32; i++) {
+			pages[i] = rnd.nextInt(32);
+			Table.add(new Record(i));
+		}		
 		
-	private static void implementThreads(Process process) {
-		if (!(process.maxTime > 0)) {
-			System.out.println("Выделенный квант времени меньше 1");
-			System.exit(0);
-		}
-		System.out.println();
-		System.out.println(process.getDescription() + "  Время макс: " + process.maxTime);	
-		int initialThreadsCount = process.Threads.size();
-		int currentThreadsCount = process.Threads.size();	
-			
-		for (int curThreadNum = 0; curThreadNum < currentThreadsCount; ) {	
-			Thread thread = process.Threads.get(curThreadNum);			
-			while (thread.needTime()) {
-					if(thread.haveTime()) {
-						thread.executeThread();		
-						process.currentTime++;
-					}
-					else {
-						System.out.println("Максимальное время " + "Поток " + (initialThreadsCount-currentThreadsCount) + " истекло");	//process.getDescription()					
-						break;
-					}
-				}				
-				System.out.println(thread.getDescription() + " завершен");				
-				process.Threads.remove(curThreadNum);
-				currentThreadsCount --;
-				
-		}
+		MemoryManager manager = new MemoryManager();	
 		
+        for(int i = 0; i < pages.length; i++){ // проходим по всем виртуальным страницам процесса
+        	manager.implementPage(pages[i], Table);//обрабатывает каждую страницу
+        	System.out.println("обращение к адресу " + pages[i]);
 	}
+    }	
 }
 
