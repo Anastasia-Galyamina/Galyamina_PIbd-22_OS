8f11712fed9d37dfc4fc89669845f1287711e25e
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2020-06-03 14:25:28 +0400

    Add files via upload

---
+++ src/lab4_nodes/DrawingManager.java
@@ -0,0 +1,42 @@
+package lab4_nodes;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+//класс, отвечающий за отрисовку
+public class DrawingManager {	
+	int cellHeight = 20;
+    int cellWidth = 20;
+	int chsize = 0;
+    int left = 0;
+    int top = 0;
+	
+	Memory memory = new Memory();
+	int count = Memory.count;
+	
+	public void Draw(Graphics g, int width, int height) {        
+       
+        for (int i = 0; i < count; i++) {
+            left = chsize * cellWidth;
+            chsize++;
+            if (Memory.nodes.get(i).getId() == -1) {
+                g.setColor(Color.GRAY);
+            } else {
+                g.setColor(Color.BLUE);
+            }
+            if (memory.nodes.get(i).getSelectFlag()) {
+                g.setColor(Color.RED);
+            }
+            g.fillRect(left, top, cellWidth, cellHeight);
+            g.setColor(Color.WHITE);
+            g.drawRect(left, top, cellWidth, cellHeight);
+            g.drawString(Memory.nodes.get(i).getId()+1 + "", left + 5, top + 20);
+            if (chsize == 20) {
+                top += cellHeight;
+                chsize = 0;
+                left = 0;
+            }
+        }
+    }
+	
+}
